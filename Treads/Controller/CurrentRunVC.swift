b4ea2c9e27c0a3ba3b630e2131021868fa49a630
Tomasz Klocek <tomek@klocek.info>
2019-12-05 22:59:39 +0100

    current runVC UI setup ver2

---
+++ Treads/Controller/CurrentRunVC.swift
@@ -0,0 +1,30 @@
+//
+//  CurrentRunVC.swift
+//  Treads
+//
+//  Created by Tomek Klocek on 2019-12-05.
+//  Copyright © 2019 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+
+class CurrentRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
