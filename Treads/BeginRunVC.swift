29d128ab35f994c45e4638871c2eb17fe93d2c99
Tomasz Klocek <tomek@klocek.info>
2019-12-05 18:54:14 +0100

    Initial Project setup and StartRunVC UI done

---
+++ Treads/BeginRunVC.swift
@@ -0,0 +1,20 @@
+//
+//  BeginRunVC.swift
+//  Treads
+//
+//  Created by Tomek Klocek on 2019-12-05.
+//  Copyright © 2019 Tomek Klocek. All rights reserved.
+//
+
+import UIKit
+
+class BeginRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
